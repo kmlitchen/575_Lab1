f08b9670ec9ad5be31d17fcc4b00caaebe1c1a98
kmlitchen <litchen@wisc.edu>
2026-03-06 16:48:18 -0600

    has all the lab 1 things ~

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-/* lab activity 6 w/ Airports.geojson */
+// ~Lab_1 w/ Airports.geojson~ //
 
 // variables declared in global scope:
 var map;
-var minValue;
+var stats = {};
 
-// fx to initiate Leaflet map
+// fx for importing GeoJSON data: 
+function getData(){
+    // load airport data
+    fetch("data/Airports.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        //call fx for: data -> stats -> symbols -> slider -> legend
+        .then(function(json){ 
+            var attributes = processData(json);
+            calcStats(json);
+            createPropSymbols(json, attributes); 
+            createSequenceControls(attributes);
+            createLegend(attributes);
+        })
+};
+
+// fx to initiate Leaflet map:
 function createMap(){
     map = L.map('map', {
         center: [40, -100],
@@ -18,89 +35,7 @@ function createMap(){
     getData(map);
 };
 
-// fx to pull min value for dataset
-function calcMinValue(data){
-    // empty array for all values
-    var allValues = [];
-    // loop through ea airport
-    for(var airport of data.features){
-        // for each, loop through ea year
-        for(var year = 2000; year <= 2024; year+=4){
-              // get value for current year and push to array
-              var value = airport.properties[String(year)];
-              allValues.push(value);
-        }
-    }
-    //get min value from array
-    var minValue = Math.min(...allValues)
-    return minValue;
-}
-
-// fx to calc radius of ea proportional symbol
-function calcPropRadius(attValue) {
-    // constant factor to adj symbol sizes evenly
-    var minRadius = 3;
-    // Flannery Appearance Compensation formula:
-    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
-    return radius;
-};
-
-// fx to convert markers to circle markers + pop-up info
-function pointToLayer(feature, latlng, attributes){
-    // attribute value to pull for symbols
-    var attribute = attributes[0];
-    // marker options
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#0084ffd7",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.2
-    };
-    // for ea feature, set attribute value - circle marker radius based on attribute value
-    var attValue = Number(feature.properties[attribute]);
-    geojsonMarkerOptions.radius = calcPropRadius(attValue);
-    // create circle marker layer
-    var layer = L.circleMarker(latlng, geojsonMarkerOptions);
-    // popup content text string
-    var popupContent = "<p><b>Airport:</b> " + feature.properties.Airport + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p>";
-    // bind popup to the circle marker, offset by radius
-    layer.bindPopup(popupContent, {
-        offset: new L.Point(0,-geojsonMarkerOptions.radius) 
-    });
-    // return circle marker to L.geoJson pointToLayer option
-    return layer;
-};
-
-// fx to add point feature circle markers to map
-function createPropSymbols(data, attributes){
-    // make Leaflet GeoJSON layer + add to map
-    L.geoJson(data, {
-        pointToLayer: function(feature, latlng){
-            return pointToLayer(feature, latlng, attributes);
-        }
-    }).addTo(map);
-};
-
-// fx to resize proportional symbols by current attribute value
-function updatePropSymbols(attribute){
-    map.eachLayer(function(layer){
-        if (layer.feature && layer.feature.properties[attribute]){
-            // local variable for current feature properties
-            var props = layer.feature.properties;
-            // update radius per new attribute values
-            var radius = calcPropRadius(props[attribute]);
-            layer.setRadius(radius);
-            // update pop-up content
-            var popupContent = "<p><b>Airport:</b> " + props.Airport + "</p><p><b>" + attribute + ":</b> " + props[attribute] + "</p>";           
-            popup = layer.getPopup();            
-            popup.setContent(popupContent).update();
-        };
-    });
-};
-
-// fx to store attribute data in an array for use in other fx
+// fx to store attribute data [in array] for use in other fx:
 function processData(data){
     // empty array for attributes
     var attributes = [];
@@ -115,29 +50,137 @@ function processData(data){
     return attributes;
 };
 
-// fx for slider to sequence data
-function createSequenceControls(attributes){
-    // create slider - insert into panel
-    var slider = "<input class='range-slider' type='range'></input>";
-    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+// fx to pull relevant stats for dataset:
+function calcStats(data){
+    // empty array for all values
+    var allValues = [];
+    // loop through ea airport
+    for(var airport of data.features){
+        // for each, loop through ea year
+        for(var year = 2000; year <= 2024; year+=4){
+              // get value for current year and push to array
+              var value = airport.properties[String(year)];
+              allValues.push(value);
+        }
+    }
+    // get min/max value from array
+    stats.min = Math.min(...allValues)
+    console.log(stats.min)
+    stats.max = Math.max(...allValues)
+    console.log(stats.max)
+    // calc mean value from array
+    var sum = allValues.reduce(function (a, b) {
+    return a + b;
+    });
+    stats.mean = sum / allValues.length;
+};
+
+// fx to calc radius of ea proportional symbol:
+function calcPropRadius(attValue) {
+    // constant factor to adj symbol sizes evenly
+    var minRadius = 3.2;
+    // Flannery Appearance Compensation formula 
+    var radius = 1.0083 * Math.pow(attValue/stats.min,0.5715) * minRadius
+    return radius;
+};
+
+// fx to populate pop-up content (refactored):
+function createPopupContent(properties, attribute){
+    var popupContent = "<p><b>Scheduled Services:</b> </p> <p><b>Airport Code: </b>" + properties.Airport + "</p><p><b>" + attribute + ":</b> " + properties[attribute] + " million </p>";
+    return popupContent;
+}
+
+// fx to convert to circle markers + pop-up info: 
+function pointToLayer(feature, latlng, attributes){
+    // attribute value to pull for symbols at ea pt
+    var attribute = attributes[0];
+    // marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#0084ffd7",
+        color: "#383737",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.2
+    };
+    // for ea feature, set attribute value - circle marker radius based on value
+    var attValue = Number(feature.properties[attribute]);
+    geojsonMarkerOptions.radius = calcPropRadius(attValue);
+    // create circle marker layer
+    var layer = L.circleMarker(latlng, geojsonMarkerOptions);
+    // popup content text string
+    var popupContent = createPopupContent(feature.properties, attribute);
+    // bind popup to marker, offset by radius
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-geojsonMarkerOptions.radius) 
+    });
+    return layer;
+};
+
+// fx to add point feature circle markers to map:
+function createPropSymbols(data, attributes){
+    // make Leaflet GeoJSON layer + add to map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+// fx to resize proportional symbols by current attribute value (year):
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            // local variable for current feature properties
+            var props = layer.feature.properties;
+            // update radius per new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+            // update pop-up content
+            var popupContent = createPopupContent(props, attribute);           
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
+        };
+    });
+    // update temporal legend
+    document.querySelector("span.year").innerHTML = attribute; 
+};
+
+// fx for slider bar:
+function createSequenceControls(attributes){   
+    // custom leaflet ctrl for slider
+    var SequenceControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
+        // add html elements, event listeners directly on-add
+        onAdd: function () {
+            // new control container div + class name
+            var container = L.DomUtil.create('div', 'sequence-control-container');
+            // make a new range input element (slider-bar)
+            container.insertAdjacentHTML('beforeend', '<input class="range-slider" type="range">')
+            // add png icons as skip buttons to container
+            container.insertAdjacentHTML('beforeend', '<button class="step" id="reverse" title="backward"><img src="img/backward.png"></button>'); // Arrow by Ghiyats Mujtaba
+            container.insertAdjacentHTML('beforeend', '<button class="step" id="forward" title="forward"><img src="img/forward.png"></button>'); // Arrow by Ghiyats Mujtaba
+            // disable event listeners when clicking within container
+            L.DomEvent.disableClickPropagation(container);
+            return container;
+        }
+    });
+    // add slider container to the map
+    map.addControl(new SequenceControl());
     // set slider attributes
     document.querySelector(".range-slider").max = 6;
     document.querySelector(".range-slider").min = 0;
     document.querySelector(".range-slider").value = 0;
     document.querySelector(".range-slider").step = 1;
-    // add buttons to slider
-    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse"></button>');
-    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
-    // set button to png icons
-    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/backward.png'>")
-    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>") // Next and Back icons by Thea Graph from Noun Project (CC BY 3.0)
     // internal fx to move slider <--> increment by button w/ circular looping
     var steps = document.querySelectorAll('.step');
     steps.forEach(function(step){
         step.addEventListener("click", function(){
             var index = document.querySelector('.range-slider').value;
-            if (step.id == 'forward'){ // forward button goes forward; highest -> back to 0
-                index++; 
+            if (step.id == 'forward'){  // forward button goes forward; highest -> back to 0
+                index++;
                 index = index > 6 ? 0 : index;
             } else if (step.id == 'reverse'){ // reverse button goes backwards; lowest <- up to 6
                 index--;
@@ -149,28 +192,49 @@ function createSequenceControls(attributes){
             updatePropSymbols(attributes[index]);
         })
     })
-
-    // internal fx for event listener to update proportional symbol by current slider pos value
-    document.querySelector('.range-slider').addEventListener('input', function(){    
-        var index = this.value;        
-        updatePropSymbols(attributes[index]);
-    });
 };
 
-// fx for importing GeoJSON data
-function getData(){
-    // load airport data
-    fetch("data/Airports.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
-            var attributes = processData(json);
-            minValue = calcMinValue(json);
-            createPropSymbols(json, attributes); //call function to create proportional symbols
-            createSequenceControls(attributes);
-        })
-};
+// fx for legend: 
+function createLegend(attributes) {
+    // custom leaflet ctrl for legend
+    var LegendControl = L.Control.extend({
+    options: {
+        position: "bottomright",
+    },
+    // add html elements, event listeners directly on-add
+    onAdd: function () {
+        // new control container div + class name
+        var container = L.DomUtil.create("div", "legend-control-container");
+        // make a new temporal legend 
+        container.innerHTML = '<p class="temporalLegend">Scheduled Services in <span class="year">2000</span></p>';
+        // start svg string for content
+        var svg = '<svg id="attribute-legend" width="160px" height="60px">';
+        // new array for looping through values from stats calc fx
+        var circles = ["max", "mean", "min"];
+        // loop through the array, for ea 
+        for (var i = 0; i < circles.length; i++) {
+        // calculate radius (r) and center (cy)
+            var radius = calcPropRadius(stats[circles[i]]);
+            console.log(radius);
+            var cy = 55 - radius;
+            console.log(cy);
+        // update svg string 
+            svg += '<circle class="legend-circle" id="' + circles[i] + '" r="' + radius + '"cy="' + cy + '" fill="#0084ffd7" opacity="0.8" stroke="#000000" cx="35"/>';  // cx to change <-> pos of circle mkrs
+            // evenly space out labels            
+            var textY = i * 20 + 20;            
+            // add legend text to svg string            
+            svg += '<text id="' + circles[i] + '-text" x="65" y="' + textY + '">' + Math.round(stats[circles[i]]*100)/100 + " million" + '</text>';
+        };
+        // after loop, close svg string
+        svg += "</svg>";
+        //add svg to legend container
+        container.insertAdjacentHTML('beforeend',svg);
+        return container;
+    },
+  });
+// add the legend container to the map
+  map.addControl(new LegendControl());
+}
 
-// map map map! 
-document.addEventListener('DOMContentLoaded',createMap)
+// map, map, map! 
+document.addEventListener('DOMContentLoaded',createMap) // activate map fx -> calls get data -> calls sub fx to process, stats, add map elements
